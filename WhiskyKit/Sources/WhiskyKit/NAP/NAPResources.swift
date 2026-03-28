0ad46382a677f492cab593c545a2ea03a0c6a86d
Me <mini@mini.local>
2026-03-29 00:37:09 +0800

    feat(nap): isolate DXMT patch logic

---
+++ WhiskyKit/Sources/WhiskyKit/NAP/NAPResources.swift
@@ -0,0 +1,24 @@
+//
+//  NAPResources.swift
+//  WhiskyKit
+//
+
+import Foundation
+
+public enum NAPResources {
+    private static var fm: FileManager { FileManager.default }
+
+    public static var rootDir: URL {
+        WhiskyPaths.applicationSupportRoot.appending(path: "NAP", directoryHint: .isDirectory)
+    }
+
+    public static var dxmtDir: URL { rootDir.appending(path: "dxmt", directoryHint: .isDirectory) }
+
+    public static func ensureDirs() throws {
+        for dir in [rootDir, dxmtDir] {
+            if !fm.fileExists(atPath: dir.path(percentEncoded: false)) {
+                try fm.createDirectory(at: dir, withIntermediateDirectories: true)
+            }
+        }
+    }
+}
